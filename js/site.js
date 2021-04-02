7e2d7b35bdc71ceb01fd4b50e3bd9f1fa1460f8e
Stephen Cronin <cronin4392@gmail.com>
2021-04-02 15:44:08 -0400

    set up draw function

--- js/site.js
+++ js/site.js
@@ -3,5 +3,16 @@
 // draw circle
 const body = document.body;
 const circle = document.querySelector('circle');
-body.style.setProperty('--circle-fill', '#FF0000');
 body.style.setProperty('--circle-animation-speed', '5s');
+
+draw();
+
+function draw() {
+  const time = Math.round((new Date().getTime()) / 20);
+  const color = `hsl(${time % 360}deg, 100%, 50%)`;
+  body.style.setProperty('--circle-fill', color);
+
+  window.requestAnimationFrame(() => {
+    draw();
+  });
+}
