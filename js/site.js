f13b45ce712c943f97de46a068044d81125c0c52
Stephen Cronin <cronin4392@gmail.com>
2021-04-02 14:17:14 -0400

    Initial commit with basic circle animating

---
+++ js/site.js
@@ -0,0 +1,7 @@
+// fetch weather data
+
+// draw circle
+const body = document.body;
+const circle = document.querySelector('circle');
+body.style.setProperty('--circle-fill', '#FF0000');
+body.style.setProperty('--circle-animation-speed', '5s');
