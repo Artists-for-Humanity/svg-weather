68eb4215dfa40b83630bbf20565e72cbbd1e2542
MassterKek <56857431+MassterKek@users.noreply.github.com>
2021-04-06 11:41:44 -0400

    Attempting to fetch weather data

--- js/site.js
+++ js/site.js
@@ -1,5 +1,15 @@
 // fetch weather data
 
+  let lat;
+  let long;
+
+  const api = `http://api.weatherapi.com/v1/forecast.json?key=5863755acb594078956213139202910&q=${lat},${long}&days=1`;
+
+  fetch(api)
+    .then((response) => response.json())
+    .then((data) => console.log(data));
+       
+
 // draw circle
 const body = document.body;
 const circle = document.querySelector('circle');
