875bcb8585f50023f070d973c05b341e5c224b88
MassterKek <56857431+MassterKek@users.noreply.github.com>
2021-04-08 14:28:22 -0400

    Fetch weather data (#2)
    * Attempting to fetch weather data
    
    * Weather Data retrieval seems to be working.
    
    * Color of circle now reflects current temperature.
    
    The hue of the hsl color used for the circle is now based on an input range of 9 to 91 degrees with an output range of 180 (blue) to 360 (red).
    
    * Cleaned up code and added speed feature
    
    Set animation speed based on current wind speed

--- js/site.js
+++ js/site.js
@@ -1,18 +1,55 @@
 // fetch weather data
 
-// draw circle
 const body = document.body;
 const circle = document.querySelector('circle');
-body.style.setProperty('--circle-animation-speed', '5s');
 
-draw();
+var lat = 42.3429718;
+var long = -71.0557969;
+var temp = 65;
+var temp_color = 180;
+var wind = 5;
+var wind_speed = 5;
 
+const api = `http://api.weatherapi.com/v1/forecast.json?key=5863755acb594078956213139202910&q=${lat},${long}&days=1`;
+
+fetch(api)
+  .then((response) => response.json())
+  .then((data) => {
+    console.log(data);
+    const {
+      temp_f,
+      feelslike_f,
+      humidity,
+      vis_miles,
+      wind_mph,
+      temp_c,
+    } = data.current;
+
+    temp = data.current.temp_f;
+    console.log("Temp: ", temp);
+    wind = data.current.wind_mph;
+    console.log("Wind: ", wind);
+    wind_speed = 10 - 5 * (wind / 12.3);
+    console.log("wind_speed: ", wind_speed);
+    temp_color = remapNumber(temp, 9, 91, 180, 360);
+    console.log("temp_color: ", temp_color);
+
+    body.style.setProperty('--circle-animation-speed', `${wind_speed}s`);
+
+    draw();
+  });
+
+// draw circle
 function draw() {
-  const time = Math.round((new Date().getTime()) / 20);
-  const color = `hsl(${time % 360}deg, 100%, 50%)`;
+  const color = `hsl(${temp_color}, 100%, 50%)`;
   body.style.setProperty('--circle-fill', color);
 
   window.requestAnimationFrame(() => {
     draw();
   });
+}
+
+// Remap a number within two given ranges
+function remapNumber(number, in_min, in_max, out_min, out_max) {
+  return (number - in_min) * (out_max - out_min) / (in_max - in_min) + out_min;
 }
