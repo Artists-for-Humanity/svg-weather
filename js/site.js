357572a2e2cc92c69cb89e1449477748e6e9922b
MassterKek <56857431+MassterKek@users.noreply.github.com>
2021-04-09 17:42:41 -0400

    Nothing appears in the DOM
    Still having an issue getting the circles to appear, can't figure out why they won't show.

--- js/site.js
+++ js/site.js
@@ -3,12 +3,12 @@
 const body = document.body;
 const circle = document.querySelector('circle');
 
-var lat = 42.3429718;
-var long = -71.0557969;
-var temp = 65;
-var temp_color = 180;
-var wind = 5;
-var wind_speed = 5;
+let lat = 42.3429718;
+let long = -71.0557969;
+let temp = 65;
+let temp_color = 180;
+let wind = 5;
+let wind_speed = 5;
 
 const api = `http://api.weatherapi.com/v1/forecast.json?key=5863755acb594078956213139202910&q=${lat},${long}&days=1`;
 
@@ -34,19 +34,66 @@ fetch(api)
     temp_color = remapNumber(temp, 9, 91, 180, 360);
     console.log("temp_color: ", temp_color);
 
-    body.style.setProperty('--circle-animation-speed', `${wind_speed}s`);
-
+    // body.style.setProperty('--circle-animation-speed', `${wind_speed}s`);
+    createSVG();
     draw();
   });
 
 // draw circle
 function draw() {
+  console.log("draw() called successfully");
   const color = `hsl(${temp_color}, 100%, 50%)`;
   body.style.setProperty('--circle-fill', color);
 
-  window.requestAnimationFrame(() => {
-    draw();
-  });
+  // window.requestAnimationFrame(() => {
+  //   draw();
+  // });
+}
+
+function createSVG() {
+  console.log("createSVG() called successfully");
+
+  const canvas = document.getElementById("canvas");
+  const svg = document.createElement("svg");
+  // Set viewport here
+  svg.setAttribute("viewBox", "0 0 1000 1000");
+  canvas.appendChild(svg);
+
+  let position ={rad:50, xpos:500, ypos:500};
+  
+  svg.appendChild(createCircle(position));
+  position = generatePositionValues(position);
+  svg.appendChild(createCircle(position));
+
+  position = generatePositionValues(position);
+  svg.appendChild(createCircle(position));
+
+  position = generatePositionValues(position);
+  svg.appendChild(createCircle(position));
+
+}
+
+function generatePositionValues(pre_pos) {
+  console.log("generatePositionValues() called successfully");
+  const rad = 50 - 25*Math.random();
+  const vector_min = Math.max(rad, pre_pos.rad);
+  const vector_max = rad + pre_pos.rad;
+  const vector_size = remapNumber(Math.random(), 0, 1, vector_min, vector_max);
+  const vector_angle = remapNumber(Math.random(), 0, 1, 225, 315);
+  const xpos = Math.cos(vector_angle)*vector_size;
+  const ypos = Math.sin(vector_angle)*vector_size;
+  console.log("Position values: ", rad, xpos, ypos);
+  return {rad, xpos, ypos};
+}
+
+function createCircle({rad, xpos, ypos}) {
+  console.log("createCircle() called successfully");
+  const circle = document.createElement("circle");
+  // Set circle attributes
+  circle.setAttribute("cx", xpos);
+  circle.setAttribute("cy", ypos);
+  circle.setAttribute("r", rad);
+  return circle;
 }
 
 // Remap a number within two given ranges
