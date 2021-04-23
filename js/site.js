34435ef69c31bc517cf4c4c68db4026a6ae24f56
Trevor Ward <56857431+MassterKek@users.noreply.github.com>
2021-04-23 16:53:56 -0400

    Building Clouds (#3)
    * Nothing appears in the DOM
    
    Still having an issue getting the circles to appear, can't figure out why they won't show.
    
    * Got the circles to appear
    
    * [WIP] Set up Cloud SVG building function, moving to animation
    
    Created the cloudSVG function which returns an svg element containing randomly positioned circles.
    Currently working on animating and coloring the clouds based on weather data.
    
    * [WIP] Trying to make shade of gray in clouds based on precipitation
    
    * Finished building clouds
    
    Next step is to try to stagger the animation of each circle in the svg
    
    * Cleaned up Javascript
    
    * [WIP] Removed id system for cloud svgs
    
    * Fixed up cloud building functions
    
    Ready to move onto animation.
    
    * Minor cleanup

--- js/site.js
+++ js/site.js
@@ -3,12 +3,8 @@
 const body = document.body;
 const circle = document.querySelector('circle');
 
-var lat = 42.3429718;
-var long = -71.0557969;
-var temp = 65;
-var temp_color = 180;
-var wind = 5;
-var wind_speed = 5;
+const lat = 42.3429718;
+const long = -71.0557969;
 
 const api = `http://api.weatherapi.com/v1/forecast.json?key=5863755acb594078956213139202910&q=${lat},${long}&days=1`;
 
@@ -17,36 +13,88 @@ fetch(api)
   .then((data) => {
     console.log(data);
     const {
-      temp_f,
-      feelslike_f,
-      humidity,
-      vis_miles,
-      wind_mph,
-      temp_c,
+      precip_mm: precip,
     } = data.current;
 
-    temp = data.current.temp_f;
-    console.log("Temp: ", temp);
-    wind = data.current.wind_mph;
-    console.log("Wind: ", wind);
-    wind_speed = 10 - 5 * (wind / 12.3);
-    console.log("wind_speed: ", wind_speed);
-    temp_color = remapNumber(temp, 9, 91, 180, 360);
-    console.log("temp_color: ", temp_color);
+    const num_clouds = 3;
+    const svg = createSVG();
 
-    body.style.setProperty('--circle-animation-speed', `${wind_speed}s`);
+    for (let i = num_clouds - 1; i >= 0; i--) {
+      createCloud(svg, num_clouds, cloudColor(precip + (i * 10)));
+    }
 
-    draw();
+
   });
 
 // draw circle
-function draw() {
-  const color = `hsl(${temp_color}, 100%, 50%)`;
-  body.style.setProperty('--circle-fill', color);
+// function draw() {
+//   console.log("draw() called successfully");
+//   const color = `hsl(0%, 100%, 50%)`;
+//   body.style.setProperty('--circle-fill', color);
 
-  window.requestAnimationFrame(() => {
-    draw();
-  });
+//   window.requestAnimationFrame(() => {
+//     draw();
+//   });
+// }
+
+// Generates position of new circle within bounds
+function generatePositionValues(pre_pos) {
+  console.log("generatePositionValues() called successfully");
+  const rad = 50 - 25 * Math.random();
+  const vector_min = Math.max(rad, pre_pos.rad);
+  const vector_max = rad + pre_pos.rad;
+  const vector_size = remapNumber(Math.random(), 0, 1, vector_min, vector_max);
+  const vector_angle = remapNumber(Math.random(), 0, 1, 0, 360);
+  const xpos = 250 + Math.sin(vector_angle) * vector_size;
+  const ypos = 250 + Math.cos(vector_angle) * vector_size;
+  console.log("Position values: ", rad, xpos, ypos);
+  return {
+    rad,
+    xpos,
+    ypos
+  };
+}
+
+// Creates an svg containing n circles
+function createSVG() {
+
+  const canvas = document.getElementById('canvas');
+  const svg = document.createElementNS("http://www.w3.org/2000/svg", "svg");
+  svg.setAttribute("viewBox", "0 0 500 500");
+  svg.setAttribute("xmlns", "http://www.w3.org/2000/svg");
+  svg.setAttribute("class", "cloud_svg");
+  canvas.appendChild(svg);
+
+  return svg;
+}
+
+function createCloud(svg, num_clouds, color) {
+  let position = {
+    rad: 50,
+    xpos: 500,
+    ypos: 500
+  };
+  for (i = 0; i < num_clouds; i++) {
+    position = generatePositionValues(position);
+    svg.innerHTML += createCircle(position, color);
+  }
+}
+
+// Returns a circle with given position and radius data
+function createCircle({
+  rad,
+  xpos,
+  ypos
+}, color) {
+  console.log("createCircle() called successfully");
+  return `<circle cx="${xpos}px" cy="${ypos}px" r="${rad}px" fill="${color}"></circle>`;
+}
+
+// Sets hsl color for cloud
+function cloudColor(precipitation) {
+  let c = remapNumber(precipitation, 0, 200, 25, 85);
+  let col_val = 110 - c;
+  return `hsl(0, 0%, ${col_val}%)`;
 }
 
 // Remap a number within two given ranges
